55995db9192c9f1cb3f9bae6f67dd345a9f06fde
Hauke <haukesa@web.de>
2017-09-14 20:46:13 -0400

    JSON easy filter

--- index.js
+++ index.js
@@ -10,20 +10,34 @@ const webhook = require('jovo-framework').Webhook;
 
 // Listen for post requests
 webhook.listen(3000, function() {
-    console.log('Local development server listening on port 3000.');
+  console.log('Local development server listening on port 3000.');
 });
 
 webhook.post('/webhook', function(req, res) {
-    app.handleRequest(req, res, handlers);
-    app.execute();
+  app.handleRequest(req, res, handlers);
+  app.execute();
 });
 
 
 var GoogleSearch = require('google-search');
 var googleSearch = new GoogleSearch({
-  key: 'AIzaSyDUiUfVFoJbsuldhvH1PeTkBCiecQycSzI',
-  cx: '015082794420679756793:m1mbjbtad9o'
+  key: '',
+  cx: ''
 });
+
+var JefNode = require('json-easy-filter').JefNode;
+
+// =================================================================================
+// Strings, variables & stuff
+// =================================================================================
+
+var askUrl = [
+    "Where do you want to search?",
+    "On which website do you want to search?",
+    "On which domain do you want to search?",
+    "Tell me on which website you want to search.",
+    "On which website?"
+];
 
 //// Log the JSON request/response to ngrok console
 // app.enableRequestLogging();
@@ -36,24 +50,91 @@ var googleSearch = new GoogleSearch({
 
 let handlers = {
 
-    'LAUNCH': function() {
+    'LAUNCH': function () {
         app.toIntent('search-website');
     },
 
-    'search-website': function(url, anyQuery) {
-
+    'search-website': function (url, domain, anyQuery) {
+        if (url === "" && domain === "") {
+            var randomNumber = Math.floor(Math.random() * askUrl.length);
+            app.setSessionAttribute('anyQuery', anyQuery);
+            app.ask(askUrl[randomNumber]);
+        }
         googleSearch.build({
-          q: anyQuery.toString(),
-          start: 5,
-          fileType: "",
-          gl: "", //geolocation,
-          lr: "lang_en",
-          num: 10, // Number of search results to return between 1 and 10, inclusive
-          siteSearch: url.toString() // Restricts results to URLs from a specified site
-        }, function(error, response) {
-          console.log(response);
+            q: anyQuery,
+            start: 1,
+            fileType: "",
+            gl: "", //location,
+            lr: "lang_en",
+            num: 10, // Number of search results to return between 1 and 10, inclusive
+            siteSearch: "http://www." + url // Restricts results to URLs from a specified site
+        }, function (error, response) {
+            filter(response)
         });
 
-          app.tell(url + anyQuery);
+        function filter(data) {
+            var title = new JefNode(data).filter(function (node) {
+                if (node.has('htmlTitle')) {
+                    //return an object instead here
+                    var str = node.value.title;//+ ' ' + node.value.link;
+                    str = str.replaceAll(url, ' ');
+                    return str
+                }
+            });
+
+            console.log(title);
+            //app.tell(title[1].toString());
+            app.tell("I searched");
+            console.log(JSON.stringify(data, null, 2));
+        }
+    },// end of 'search-website'
+
+    //complete search
+    'url-domain': function (url, domain) {
+        var search = "";
+        if (url !== ""){
+            search = url;
+        }
+        else {
+            search = domain;
+        }
+        let query = app.getSessionAttribute('anyQuery');
+        googleSearch.build({
+            q: query,
+            start: 1,
+            fileType: "",
+            gl: "", //location
+            lr: "lang_en",
+            num: 10, // Number of search results to return between 1 and 10, inclusive
+            siteSearch: "http://www." + search // Restricts results to URLs from a specified site
+        }, function (error, response) {
+            filter(response)
+        });
+
+        function filter(data) {
+            var title = new JefNode(data).filter(function (node) {
+                if (node.has('htmlTitle')) {
+                    //return an object instead here
+                    var str = node.value.title;//+ ' ' + node.value.link;
+                    str = str.replaceAll(url, ' ');
+                    return str
+                }
+            });
+            console.log(title);
+            //app.tell(title[1].toString());
+            app.tell("I searched");
+            console.log(JSON.stringify(data, null, 2));
+        }
+
     }
 };
+
+
+String.prototype.replaceAll = function(strReplace, strWith) {
+    // See http://stackoverflow.com/a/3561711/556609
+    var esc = strReplace.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+    var reg = new RegExp(esc, 'ig');
+    return this.replace(reg, strWith);
+};
+
+
