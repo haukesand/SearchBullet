694afbfc74364ea3517832cade0e6d37bded8a76
Hauke <haukesa@web.de>
2017-09-14 20:46:13 -0400

    JSON easy filter

--- index.js
+++ index.js
@@ -10,20 +10,22 @@ const webhook = require('jovo-framework').Webhook;
 
 // Listen for post requests
 webhook.listen(3000, function() {
-    console.log('Local development server listening on port 3000.');
+  console.log('Local development server listening on port 3000.');
 });
 
 webhook.post('/webhook', function(req, res) {
-    app.handleRequest(req, res, handlers);
-    app.execute();
+  app.handleRequest(req, res, handlers);
+  app.execute();
 });
 
 
 var GoogleSearch = require('google-search');
 var googleSearch = new GoogleSearch({
-  key: '***REMOVED***',
-  cx: '***REMOVED***'
+  key: 'AIzaSyDUiUfVFoJbsuldhvH1PeTkBCiecQycSzI',
+  cx: '015082794420679756793:m1mbjbtad9o'
 });
+
+var JefNode = require('json-easy-filter').JefNode;
 
 //// Log the JSON request/response to ngrok console
 // app.enableRequestLogging();
@@ -36,24 +38,33 @@ var googleSearch = new GoogleSearch({
 
 let handlers = {
 
-    'LAUNCH': function() {
-        app.toIntent('search-website');
-    },
+  'LAUNCH': function() {
+    app.toIntent('search-website');
+  },
 
-    'search-website': function(url, anyQuery) {
+  'search-website': function(url, anyQuery) {
 
-        googleSearch.build({
-          q: anyQuery.toString(),
-          start: 5,
-          fileType: "",
-          gl: "", //geolocation,
-          lr: "lang_en",
-          num: 10, // Number of search results to return between 1 and 10, inclusive
-          siteSearch: url.toString() // Restricts results to URLs from a specified site
-        }, function(error, response) {
-          console.log(response);
-        });
+    googleSearch.build({
+      q: anyQuery,
+      start: 1,
+      fileType: "",
+      gl: "", //geolocation,
+      lr: "lang_en",
+      num: 10, // Number of search results to return between 1 and 10, inclusive
+      siteSearch: "http://www.chefkoch.de" // Restricts results to URLs from a specified site
+    }, function(error, response) {
+      receive(response)
+    });
 
-          app.tell(url + anyQuery);
+    function receive(data) {
+      var title = new JefNode(data).filter(function(node) {
+        if (node.has('htmlTitle')) {
+          return node.value.title;
+        }
+      });
+      console.log(title);
+      console.log(JSON.stringify(data, null, 2));
     }
+    app.tell(url + anyQuery);
+  }
 };
