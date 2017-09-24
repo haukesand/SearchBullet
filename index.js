1531b9bf9ce2821295e22b6e26cad80dc633393c
Hauke <haukesa@web.de>
2017-09-24 01:22:30 -0400

    Simplify code

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ const app = require('jovo-framework').Jovo;
 const webhook = require('jovo-framework').Webhook;
 
 // Listen for post requests
-webhook.listen(3000, function() {
-  console.log('Local development server listening on port 3000.');
+webhook.listen(3000, function () {
+    console.log('Local development server listening on port 3000.');
 });
 
-webhook.post('/webhook', function(req, res) {
-  app.handleRequest(req, res, handlers);
-  app.execute();
+webhook.post('/webhook', function (req, res) {
+    app.handleRequest(req, res, handlers);
+    app.execute();
 });
 
 
 var GoogleSearch = require('google-search');
 var googleSearch = new GoogleSearch({
-  key: '',
-  cx: ''
+    key: '',
+    cx: ''
 });
 
 var JefNode = require('json-easy-filter').JefNode;
@@ -60,77 +60,53 @@ let handlers = {
             app.setSessionAttribute('anyQuery', anyQuery);
             app.ask(askUrl[randomNumber]);
         }
-        googleSearch.build({
-            q: anyQuery,
-            start: 1,
-            fileType: "",
-            gl: "", //location,
-            lr: "lang_en",
-            num: 10, // Number of search results to return between 1 and 10, inclusive
-            siteSearch: "http://www." + url // Restricts results to URLs from a specified site
-        }, function (error, response) {
-            filter(response)
-        });
-
-        function filter(data) {
-            var title = new JefNode(data).filter(function (node) {
-                if (node.has('htmlTitle')) {
-                    //return an object instead here
-                    var str = node.value.title;//+ ' ' + node.value.link;
-                    str = str.replaceAll(url, ' ');
-                    return str
-                }
-            });
-
-            console.log(title);
-            //app.tell(title[1].toString());
-            app.tell("I searched");
-            console.log(JSON.stringify(data, null, 2));
-        }
+        finalizeGoogleSearch(url, domain, anyQuery);
     },// end of 'search-website'
 
     //complete search
     'url-domain': function (url, domain) {
-        var search = "";
-        if (url !== ""){
-            search = url;
-        }
-        else {
-            search = domain;
-        }
         let query = app.getSessionAttribute('anyQuery');
-        googleSearch.build({
-            q: query,
-            start: 1,
-            fileType: "",
-            gl: "", //location
-            lr: "lang_en",
-            num: 10, // Number of search results to return between 1 and 10, inclusive
-            siteSearch: "http://www." + search // Restricts results to URLs from a specified site
-        }, function (error, response) {
-            filter(response)
-        });
-
-        function filter(data) {
-            var title = new JefNode(data).filter(function (node) {
-                if (node.has('htmlTitle')) {
-                    //return an object instead here
-                    var str = node.value.title;//+ ' ' + node.value.link;
-                    str = str.replaceAll(url, ' ');
-                    return str
-                }
-            });
-            console.log(title);
-            //app.tell(title[1].toString());
-            app.tell("I searched");
-            console.log(JSON.stringify(data, null, 2));
-        }
-
+        finalizeGoogleSearch(url, domain, query);
     }
-};
 
+}
 
-String.prototype.replaceAll = function(strReplace, strWith) {
+function filter(data, replace) {
+    var title = new JefNode(data).filter(function (node) {
+        if (node.has('htmlTitle')) {
+            //return an object instead here
+            var str = node.value.title;//+ ' ' + node.value.link;
+            str = str.replaceAll(replace, ' ');
+            return str
+        }
+    });
+    return title;
+}
+function finalizeGoogleSearch(url, domain, query){
+    var search = "";
+    if (url !== "") {
+        search = url;
+    }
+    else {
+        search = domain;
+    }
+    googleSearch.build({
+        q: query,
+        start: 1,
+        fileType: "",
+        gl: "", //location
+        lr: "lang_en",
+        num: 10, // Number of search results to return between 1 and 10, inclusive
+        siteSearch: "http://www." + search // Restricts results to URLs from a specified site
+    }, function (error, response) {
+        var title = filter(response, url);
+        console.log(title);
+        //app.tell(title[1].toString());
+        app.tell("I searched");
+        console.log(JSON.stringify(response, null, 2));
+    });
+}
+String.prototype.replaceAll = function (strReplace, strWith) {
     // See http://stackoverflow.com/a/3561711/556609
     var esc = strReplace.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
     var reg = new RegExp(esc, 'ig');
